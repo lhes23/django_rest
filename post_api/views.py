bce088a180d3462381b8372e3a69ab3619684ff6
Lester <32542078+lhes23@users.noreply.github.com>
2022-09-26 20:37:37 +0800

    use Response instead of JsonResponse

--- post_api/views.py
+++ post_api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework import status
+from rest_framework.response import Response
 
 from post_api.models import Post
 from post_api.serializers import PostSerializer
@@ -13,10 +14,11 @@ def getAllPosts(request):
         case 'GET':
             posts = Post.objects.all()
             serializer = PostSerializer(posts, many=True)
-            return JsonResponse({'posts': serializer.data})
+            return Response({'posts': serializer.data})
         case 'POST':
             serializer = PostSerializer(data=request.data)
             if serializer.is_valid():
                 serializer.save()
-                return JsonResponse({'post': serializer.data}, status=status.HTTP_202_ACCEPTED )
-            return JsonResponse({'errors': serializer.errors}, status=status.HTTP_400_BAD_REQUEST )
+                return Response({'post': serializer.data}, status=status.HTTP_202_ACCEPTED )
+            return Response({'errors': serializer.errors}, status=status.HTTP_400_BAD_REQUEST )
+
