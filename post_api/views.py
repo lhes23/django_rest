b6fdc216633777763ee17863d807a07114b8ae17
Lester <32542078+lhes23@users.noreply.github.com>
2022-09-26 20:15:35 +0800

    get all posts serializers

--- post_api/views.py
+++ post_api/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 
+from post_api.models import Post
+from post_api.serializers import PostSerializer
+
 # Create your views here.
 @api_view(['GET'])
 def getAllPosts(request):
-    return JsonResponse("Hello World!", safe=False)
+    posts = Post.objects.all()
+    serializer = PostSerializer(posts, many=True)
+    return JsonResponse({'posts': serializer.data})
