5c9ea6e211dcaee05622c746494f0ae7c7c2492f
Lester <32542078+lhes23@users.noreply.github.com>
2022-09-26 20:33:28 +0800

    add post request

--- post_api/views.py
+++ post_api/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
+from rest_framework import status
 
 from post_api.models import Post
 from post_api.serializers import PostSerializer
 
 # Create your views here.
-@api_view(['GET'])
+@api_view(['GET','POST'])
 def getAllPosts(request):
-    posts = Post.objects.all()
-    serializer = PostSerializer(posts, many=True)
-    return JsonResponse({'posts': serializer.data})
+    match(request.method):
+        case 'GET':
+            posts = Post.objects.all()
+            serializer = PostSerializer(posts, many=True)
+            return JsonResponse({'posts': serializer.data})
+        case 'POST':
+            serializer = PostSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return JsonResponse({'post': serializer.data}, status=status.HTTP_202_ACCEPTED )
+            return JsonResponse({'errors': serializer.errors}, status=status.HTTP_400_BAD_REQUEST )
